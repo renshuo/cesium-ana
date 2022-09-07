9ec996b178e6d39c005b3ba5d4d11476bb6f8fc5
renshuo <renshuoo@qq.com>
2022-09-07 15:26:53 +0800

    In the beginning there was darkness

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div style="position: relative">
+    <div style="position: fixed; z-index: 10; left: 20px; top: 20px; color: white">
+      <!-- <button class="tbt" @click="">大气层</button> -->
+    </div>
+
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+window.CESIUM_BASE_URL = "/Cesium";
+import * as Cesium from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+import CesiumAna from './ana/index.ts'
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNWY5YzhhMS05ZmYxLTQ5NzgtOTcwNC0zZmViNGFjZjc4ODEiLCJpZCI6ODU0MjMsImlhdCI6MTY0Njk4ODA1NX0.4-plF_5ZfEMMpHqJyefkDCFC8JWkFw39s3yKVcNg55c";
+  Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
+    100,
+    30,
+    110,
+    40
+  );
+  let viewer = new Cesium.Viewer("mapContainer", {
+    infoBox: false, //是否显示信息框
+    selectionIndicator: false, //是否显示选取指示器组件
+    timeline: false, //是否显示时间轴
+    animation: false, //是否创建动画小器件，左下角仪表
+    sceneModePicker: false,
+    shouldAnimate: true,
+  });
+
+  viewer.scene.camera.setView({
+    destination: new Cesium.Cartesian3(
+      277096.634865404,
+      5647834.481964232,
+      2985563.7039122293
+    ),
+    orientation: {
+      heading: 4.731089976107251,
+      pitch: -0.32003481981370063,
+    },
+  });
+
+});
+</script>
+
+<style>
+.tbt {
+  border: 1px solid blue;
+  background: transparent;
+  background-color: rgba(80,80,80, 0.7);
+  border-radius: 2px;
+  margin: 1px 2px;
+  padding: 2px 8px;
+  min-width: 90px;
+  font-size: 14px;
+  line-height: 16px;
+  color: #00ff00;
+}
+</style>
---
+++ src/ana/index.ts
@@ -0,0 +1,15 @@
+import {Viewer} from 'cesium';
+
+
+export default class CesiumAnalyzer {
+
+  private viewer: Viewer
+
+  constructor(viewer: Viewer) {
+    this.viewer = viewer
+  }
+
+  
+
+
+}
