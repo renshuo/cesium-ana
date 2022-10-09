78bd210658f21e0e71c306d99606ce1a2a2f3302
renshuo <renshuoo@qq.com>
2022-10-09 10:19:21 +0800

    使用视线分析模拟视域分析

--- src/ana/SightArea.ts
+++ src/ana/SightArea.ts
@@ -1,8 +1,8 @@
 import { Graph } from 'cesium-plotting-symbol'
-import { Viewer, Entity, Cartesian3, CallbackProperty, Color, JulianDate, HeightReference, Cartographic, PolygonHierarchy, Math } from "cesium";
+import { Viewer, Entity, Cartesian3, CallbackProperty, Color, JulianDate, HeightReference, Cartographic, PolygonHierarchy, Math, Iso8601 } from "cesium";
 import * as turf from "@turf/turf";
 import { Feature } from '@turf/turf';
-import { getSightPoints } from './SightUtil'
+import { getSightPoints, toArea } from './SightUtil'
 
 
 export default class SightArea extends Graph{
@@ -31,25 +31,34 @@ export default class SightArea extends Graph{
     })))
   }
 
-  override finish() {
-    super.finish()
-    console.log(" create sight area")
-    let p1 = this.ctlToPoint(this.ctls[0])
-    let p2 = this.ctlToPoint(this.ctls[1])
-    console.log("get point: ", p1, p2)
-    let tgts = this.getTarget(p1, p2)
-    console.log("get target : ", tgts)
-    let peaksss = tgts.map( tgt => {
-      return getSightPoints(this.ctls[0].position.getValue(JulianDate.now()), tgt, this.viewer.scene)
+
+  private drawAreas(areas) {
+    let colrs = [Color.RED, Color.GREEN]
+    areas.map((area, i) => {
+      area.map(pg => {
+        let poses = pg.map(p => {
+          let co = p.point
+          //co.height = co.height + 10
+          return Cartographic.toCartesian(co, this.viewer.scene.globe.ellipsoid, new Cartesian3())
+        })
+        this.entities.add(new Entity({
+          polygon: {
+            fill: true,
+            material: colrs[i],
+            hierarchy: new PolygonHierarchy(poses, []),
+            //perPositionHeight: true
+            heightReference: HeightReference.CLAMP_TO_GROUND
+          }
+        }))
+      })
     })
+  }
 
-    console.log("get peaks : ", peaksss.map( peakss => peakss.flat()))
-
+  private drawAreaPoints(peaksss) {
     let peaks = peaksss.flat().flat()
 
-    let inSightArea = peaks.filter( p => p.inSight === true)
-    let outSight = peaks.filter( p => p.inSight === false)
-
+    let inSightArea = peaks.filter(p => p.inSight === true)
+    let outSight = peaks.filter(p => p.inSight === false)
 
     peaks.map( pk => {
       let color = pk.inSight ? Color.GREEN : Color.RED
@@ -61,19 +70,36 @@ export default class SightArea extends Graph{
         position: Cartographic.toCartesian(pk.point, this.viewer.scene.globe.ellipsoid, new Cartesian3())
       }))
     })
-
-
   }
+
+  override finish() {
+    super.finish()
+    console.log(" create sight area")
+    let p1 = this.ctlToPoint(this.ctls[0])
+    let p2 = this.ctlToPoint(this.ctls[1])
+    console.log("get point: ", p1, p2)
+    let tgts = this.getTarget(p1, p2)
+    console.log("get target : ", tgts)
+    let origin = this.ctls[0].position.getValue(JulianDate.now())
+    let peaksss = tgts.map( (tgt,i) => {
+      return getSightPoints(origin, tgt, this.viewer.scene, i)
+    })
+    let areas = toArea(peaksss)
+    this.drawAreas(areas)
+    //this.drawAreaPoints(peaksss)
+  }
+
+  sightRadian:number = 60 //视角范围角度
+  sightArcSteps = 100 // 视角采样数量
 
   private getTarget(center: Feature<turf.Point>, p1: Feature<turf.Point>) {
     var radius = turf.distance(center, p1, { unites: 'meters' });
     var bear0 = turf.bearing(center, p1)
-    var arc = turf.lineArc(center, radius, bear0-30, bear0+30);
+    var arc = turf.lineArc(center, radius, bear0-this.sightRadian/2, bear0+this.sightRadian/2, {steps: this.sightArcSteps * 360/this.sightRadian});
     let cood = turf.getCoords(arc)
     console.log("get cood: ", cood)
     return cood.map(pos => Cartesian3.fromDegrees(pos[0], pos[1]))
   }
-
 
   private ctlToPoint(ctl: Entity): turf.Point {
     let pos = ctl.position.getValue(JulianDate.now())
--- src/ana/SightUtil.ts
+++ src/ana/SightUtil.ts
@@ -1,15 +1,17 @@
 import { Cartesian3, Cartographic, Scene } from "cesium"
 import * as R from "ramda"
-
+import * as turf from "@turf/turf"
 
 export interface SightLinePoint {
   point: Cartographic,
+  group: number,
   index: number,
   slope: number,
   inSight: boolean
 }
 
 export function getSightPoints(opos: Cartesian3, dpos: Cartesian3, scene: Scene,
+                               group: number = 0, // 视线分组
                                step: number = 100, // // 视线的切分数量
                                humanHeight: number = 2 // 原点的人身高度
                               ): Array<Array<SightLinePoint>> {
@@ -18,7 +20,7 @@ export function getSightPoints(opos: Cartesian3, dpos: Cartesian3, scene: Scene,
     let pos = Cartesian3.lerp(opos, dpos, i / step, new Cartesian3())
     let co: Cartographic = Cartographic.fromCartesian(pos)
     co.height = scene.globe.getHeight(co)
-    intPos.push({ point: co, index: i, slope: 0, inSight: true })
+    intPos.push({ point: co, group, index: i, slope: 0, inSight: true })
   }
 
   // 对视点的斜率，相对于初始斜率，变大或不变则可见，变小则不可见
@@ -37,6 +39,32 @@ export function getSightPoints(opos: Cartesian3, dpos: Cartesian3, scene: Scene,
 
   let sightgp = R.groupWith((pa: SightLinePoint, pb: SightLinePoint) => pa.inSight === pb.inSight, intPos)
   return sightgp
+}
+
+
+export function toArea(points: Array<Array<Array<SightLinePoint>>>) {
+  let grid = points.map( line => {
+    return line.flat()
+  })
+  let xmax = grid.length-1
+  let ymax = grid[0].length-1
+  let tps = points.flat().flat().map( slp  => {
+    return turf.point([slp.group, slp.index], { inSight: slp.inSight ? 1 : -1, slp })
+  })
+  let coll = turf.featureCollection(tps)
+  console.log('get coll: ', coll)
+  let areas = turf.isobands(coll, [-2, 0, 2], {zProperty: 'inSight'})
+
+  return areas.features.map( area => {
+    return area.geometry.coordinates.map( pg => {
+      return pg[0].map(ps => {
+        let [x, y] = ps
+        let x1 = x > xmax ? xmax : Math.floor(x)
+        let y1 = y > ymax ? ymax : Math.floor(y)
+        return grid[x1][y1]
+      })
+    })
+  })
 }
 
 
